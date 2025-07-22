3145ee5d9f4ee64785c07ef50e54ea9376bc5617
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2025-07-22 15:50:30 -0300

    feat(MovieSection): cria seção de filmes com busca e lista em cartaz
    - Criação do componente `MovieSection` com estrutura inicial e estilos em `MovieSection.module.css`.
    - Inclusão de campo de busca utilizando `Fieldset`, `InputText` e `Button` com ícone `FaSearch`.
    - Adicionado título "Em Cartaz" e integração com o componente `MovieList`.
    - Criação de um array tipado `Movie[]` para renderizar filmes com segurança de tipos.
    - Atualização do `App.tsx` para exibir a nova seção abaixo do `Banner`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
-import Link from "./components/Link";
+import MovieSection from "./components/MovieSection";
 
 function App() {
   return(
   <>
     <Header/>
     <Banner src="./Imagens/banner.png" alt="Pessoas no cinema com ilustração geométrica de projetor à direita." />
-    <Link href="/" target="_blank">Link</Link>
+    <MovieSection/>
   </>
   );
 }
